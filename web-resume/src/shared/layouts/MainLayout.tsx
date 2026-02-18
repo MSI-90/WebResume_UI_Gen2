601f11c1d1d186c0a30121e971bf6f2e0e1c3bed
MSI-90 <s.miller_22@outlook.com>
2026-02-18 12:47:50 +0700

    Обновлен Vite конфиг с учетом ранее добавленных путей в ts конфиг app.ts

--- web-resume/src/shared/layouts/MainLayout.tsx
+++ web-resume/src/shared/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import Footer from "@widgets/layoutHeader/LayoutHeader";
 import Header from "@widgets/layoutFooter/LayoutFooter";
+import type {PropsWithChildren} from "react";
 
-export default function MainLayout() {
+export default function MainLayout({children}:PropsWithChildren) {
+
   return (
     <>
       <div>
         <Header />
+        {children}
         <Footer />
       </div>
     </>
---
+++ web-resume/src/widgets/fakeBody/FakeBody.tsx
@@ -0,0 +1,8 @@
+
+export default function FakeBody() {
+  return (
+    <div>
+      <h1>Hello from MainLayout</h1>
+    </div>
+  )
+}
